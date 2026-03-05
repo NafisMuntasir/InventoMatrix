f927d442e655515d50cbfaa4451faa19e9d1c899
sadiaaurthy <sadiaaurthy2005@gmail.com>
2026-03-05 22:13:58 +0600

    CPP file of DamageType Has been created

---
+++ DamageType.cpp
@@ -0,0 +1,4 @@
+#include "DamageType.hpp"
+
+// DamageType is a scoped enum class (enum class DamageType).
+// Enum classes are fully defined in the header and require no implementation in a .cpp file.
