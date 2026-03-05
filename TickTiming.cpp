82a293c407ce4c68078f1d60e7d7ec413d6a054e
sadiaaurthy <sadiaaurthy2005@gmail.com>
2026-03-05 22:16:53 +0600

    cpp file of TickTiming enum class has been Created

---
+++ TickTiming.cpp
@@ -0,0 +1,4 @@
+#include "TickTiming.hpp"
+
+// TickTiming is a scoped enum class (enum class TickTiming).
+// Enum classes are fully defined in the header and require no implementation in a .cpp file.
