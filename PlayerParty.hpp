955825a3abd96e6939553a3076ab1e8010a3e588
HASANEPSILON <hbm15113@gmail.com>
2026-03-09 15:40:26 +0600

    Update PlayerParty.hpp

--- PlayerParty.hpp
+++ PlayerParty.hpp
@@ -1,36 +1,35 @@
-#ifndef PLAYERPARTY_HPP
-#define PLAYERPARTY_HPP
+#ifndef PLAYERPARTY_H
+#define PLAYERPARTY_H
 
-#include "Party.hpp"
-#include "Character.hpp"
-#include <string>
-#include <vector>
-#include <memory>
-#include <iostream>
+#include "Party.h"
 
-class PlayerParty : public Party {
+class PlayerParty : public Party
+{
 private:
     int totalWeight;
     int partyWeightLimit;
 
 public:
-    PlayerParty(std::string name, int weightLimit = 100, int weight = 0);
+    PlayerParty(std::string name, int id, int weightLimit);
 
-    int getTotalWeight() const;
-    int getPartyWeightLimit() const;
+    int getTotalWeight();
+    int getPartyWeightLimit();
 
-    void setTotalWeight(int weight);
-    void setPartyWeightLimit(int weightLimit);
+    bool isPartyOverencumbered();
 
-    void addMember(std::shared_ptr<Character> member, int weight);
-    void updateTotalWeight(int weightChange);
-    void removeMember(std::shared_ptr<Character> member, int weight);
+    void setPartyWeightLimit(int limit);
 
-    // Overrides from Party/Character
+    bool addMember(CharPtr character) override;
+
+    void updateTotalWeight();
+
     void executeTurn(Battle& battle) override;
+
     void onTurnStart(Battle& battle) override;
+
     void onTurnEnd(Battle& battle) override;
-    bool isPlayerParty() const override;
+
+    bool isPlayerParty() override;
 };
 
 #endif
