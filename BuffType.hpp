9ae1f2e26e06b8834a12f70bf460a38748f4836c
HASANEPSILON <hbm15113@gmail.com>
2026-03-09 16:01:52 +0600

    Update BuffType.hpp

--- BuffType.hpp
+++ BuffType.hpp
@@ -1,13 +1,11 @@
-#pragma once
+#ifndef BUFFTYPE_H
+#define BUFFTYPE_H
 
-// BuffType: Categorises what aspect of a character a Buff affects.
-// Used by: Buff (stores type as a member, getType() returns it),
-//          Character (getModifiedAttack checks BuffType::AttackBuff,
-//                     getModifiedDefense checks BuffType::DefenseBuff)
-// Dependency on other classes: NONE - pure enum, no includes required.
-
-enum class BuffType {
+enum class BuffType
+{
     AttackBuff,
     DefenseBuff,
     Nerf
 };
+
+#endif
