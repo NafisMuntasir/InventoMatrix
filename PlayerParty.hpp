4b53ca57061cbfd13ac8933c520b7d54db0eccf2
HASANEPSILON <hbm15113@gmail.com>
2026-03-14 17:17:36 +0600

    Refactor PlayerParty class header file

--- PlayerParty.hpp
+++ PlayerParty.hpp
@@ -1,7 +1,11 @@
-#ifndef PLAYERPARTY_H
-#define PLAYERPARTY_H
+#ifndef PLAYERPARTY_HPP
+#define PLAYERPARTY_HPP
 
 #include "Party.hpp"
+#include <memory>
+
+class Battle;
+class Character;
 
 class PlayerParty : public Party
 {
@@ -10,26 +14,18 @@ private:
     int partyWeightLimit;
 
 public:
+
     PlayerParty(std::string name, int id, int weightLimit);
-
-    int getTotalWeight() const ;
-    int getPartyWeightLimit() const ;
-
-    bool isPartyOverencumbered();
-
+    int getTotalWeight() const;
+    int getPartyWeightLimit() const;
+    bool isPartyOverencumbered() const;
     void setPartyWeightLimit(int limit);
-
-    bool addMember(const CharPtr& character) override;
-
+    bool addMember(std::shared_ptr<Character> character);
     void updateTotalWeight();
-
     void executeTurn(Battle& battle) override;
-
     void onTurnStart(Battle& battle) override;
-
     void onTurnEnd(Battle& battle) override;
-
-    bool isPlayerParty() override;
+    bool isPlayerParty() const override;
 };
 
 #endif
