26a3e9cd5dae886cf88c75e050e3c15e8c743370
HASANEPSILON <hbm15113@gmail.com>
2026-03-14 17:32:28 +0600

    Refactor PlayerParty constructor and member functions

--- PlayerParty.hpp
+++ PlayerParty.hpp
@@ -3,6 +3,9 @@
 
 #include "Party.hpp"
 #include <memory>
+#include <string>
+
+using namespace std;
 
 class Battle;
 class Character;
@@ -15,16 +18,29 @@ private:
 
 public:
 
-    PlayerParty(std::string name, int id, int weightLimit);
+    // Constructor
+    PlayerParty(string name, int id, int weightLimit);
+
+    // Getters
     int getTotalWeight() const;
     int getPartyWeightLimit() const;
     bool isPartyOverencumbered() const;
+
+    // Setter
     void setPartyWeightLimit(int limit);
-    bool addMember(std::shared_ptr<Character> character);
+
+    // Member management
+    bool addMember(shared_ptr<Character> character);
+
+    // Weight management
     void updateTotalWeight();
+
+    // Turn handling
     void executeTurn(Battle& battle) override;
     void onTurnStart(Battle& battle) override;
     void onTurnEnd(Battle& battle) override;
+
+    // Identify party type
     bool isPlayerParty() const override;
 };
 
